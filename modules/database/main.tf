def4a628102e07e82b53491e4bfeefe785676998
aswestudio2025 <awsestudio2025@gmail.com>
2025-10-14 16:18:51 -0500

    actualizacion politica clave Secert

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -9,7 +9,7 @@
   resource "random_password" "db_master_password" {
     length           = 16
     special          = true
-    override_special = "!@#$%^&*"
+    override_special = "!#$%^&*"
     upper            = true
     lower            = true
     numeric          = true
